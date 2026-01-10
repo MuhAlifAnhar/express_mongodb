22ef34f97f153bd74ae0fb67dcf4e5d0cefbd510
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-11 06:34:41 +0800

    menambah models

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const Product = require('./models/product');
+
 mongoose.connect('mongodb://localhost/shop_db')
     .then((result) => {
         console.log('Koneksi ke database berhasil');
