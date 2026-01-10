fad9ba86291d14207621f47763de4e2172cdd3b9
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-11 07:31:08 +0800

    fix semuanya

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.get('/', (req, res) => {
     res.send('Selamat datang di API SMK Telkom Makassar');
 })
 
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products });
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+})
+
 app.listen(8080, () => {
     console.log('Server berjalan di http://localhost:8080');
 })
