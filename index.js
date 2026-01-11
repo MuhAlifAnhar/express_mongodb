0218e7a8f1189443a5cb113d15cff70e22d2e992
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-11 08:55:04 +0800

    menambah edit

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.get('/products/:id', async (req, res) => {
     res.render('products/show', { product });
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product });
+})
+
 app.listen(8080, () => {
     console.log('Server berjalan di http://localhost:8080');
 })
